bec306419354a82cb825cfcc99e59f83c5639478
DongjuPark <pdj95044@gmail.com>
2024-02-15 16:55:02 +0900

    update

--- safeArea.swift
+++ safeArea.swift
@@ -5,7 +5,7 @@
     물높이 입력(N) -> 지형지도 입력(groundHeight) 입력-> 물에 잠긴 지형 관련 binary 지도 생성 
     -> 반복(방문할 곳이 없을때 까지){안전지역 탐색 -> 방문? -> BFS 탐색}
 */
-
+//내 풀이
 import Foundation
 
 //물높이 입력
@@ -19,16 +19,15 @@ for _ in 0..<N{
 }
 
 //binary 지도 생성 1 == 안전지역, 0 == 침수지역
-var binaryMap: [[Int]] = Array(repeating: Array(repeating: 0, count: N), count: N)
+var binaryMap: [[Bool]] = Array(repeating: Array(repeating: false, count: N), count: N)
 
 for i in 0..<N{
     for j in 0..<N{
-        if N < groundHeight[i][j]{ binaryMap[i][j] = 1 }
+        if N < groundHeight[i][j]{ binaryMap[i][j] = true }
     }
 }
 
 //BFS 탐색
-
 var queue = [[Int]]()
 var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: N), count: N)
 var numOfSafeArea: Int = 0
@@ -38,7 +37,7 @@ let dy:[Int] = [0,0,1,-1]
 
 for i in 0..<N{
     for j in 0..<N{
-        if(visited[i][j] == false && binaryMap[i][j] == 1){
+        if(!visited[i][j] && binaryMap[i][j]){
             //BFS 탐색 시작
             queue.append([i,j])
             visited[i][j] = true
@@ -55,7 +54,7 @@ for i in 0..<N{
                     let ny = y + dy[z]
                     
                     if(nx >= 0 && nx < N && ny >= 0 && ny < N){
-                        if(!visited[nx][ny] && binaryMap[nx][ny] == 1){
+                        if(!visited[nx][ny] && binaryMap[nx][ny]){
                             queue.append([nx,ny])
                             visited[nx][ny] = true
                         }
