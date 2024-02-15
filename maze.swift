7df69b334a8a39e18bc27db02e79bf2c6c7a3c5f
DongjuPark <pdj95044@gmail.com>
2024-02-15 16:53:59 +0900

    각기 다른 메소드들로부터 전달 받은 반환값으로인한 map 메소드의 클로져 인자의 차이 설명

--- maze.swift
+++ maze.swift
@@ -1,4 +1,4 @@
-/* 
+/*
 [백준] 미로 탐색
 0과 1로 이루어진 미로에서 목적진까지의 최소 거리를 구하는 문제입니다
 인덱스에 해당하는 값이 1인 경우는 접근할 수 있는 공간을 의미
@@ -34,14 +34,24 @@ var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: 6), coun
 var distances: [[Int]] = Array(repeating: Array(repeating: -1, count: 6), count: 4)
 
 //목적지 좌표 입력 N(0번째 인덱스) M(1번째 인덱스)
+//map(){ Int(String($0))} == 시간 절약
 var destination: [Int] = readLine()!.components(separatedBy: " ").map(){ Int($0) ?? 0}
 
 //지도 입력
 //difficult: 띄어쓰기 구분되어 있지 않은 입력을 어떻게 처리할 수 있을까?
 /*
-    String타입에서 .map()메소드는 Charactor 타입을 반환합니다.
-    이때 Charactor타입에서 Int 타입으로 바로 typecasting을 할 수 없기 때문에 String으로 형변환 후에 Int로 형변환
-    형변환 순서: Charactor -> String -> Int
+readLine()!.map()과 readLine()!.components(seperatedBy: " ").map()의 차이
+readLine()의 반환값은 String?
+components(seperatedBy:)의 반환값은 [String]이다.
+map 메소드는 컬렉션의 각 요소들을 클로져에서 정의한 코드대로 적용하여 해당 컬렉션으로 반환하는 메소드입니다.
+그래서 readLine()으로부터 반환 받은 String의 각 요소($0)의 타입을 String.element(AKA Charactor)이고
+components()으로부터 반환 받은 [String]의 각 요소의 타입은 String이 되게 됩니다.
+
+이때 Charactor타입에서 Int 타입으로 바로 typecasting을 할 수 없기 때문에 String으로 형변환 후에 Int로 형변환
+형변환 순서: Charactor -> String -> Int
+
+이러한 이유로 readLine().map()의 클로져의 인자와 readLine().components(seperatedBy: ).map의 클로져의 인자의 타입의 차이가 발생하였고
+Int형으로의 형변화 과정에 있어 방식이 달랐습니다.
  */
 var map = [[Int]]()
 for _ in 0..<destination[0]{
@@ -50,8 +60,6 @@ for _ in 0..<destination[0]{
 
 queue.append([0,0])
 distances[0][0] = 1
-
-
 
 while !queue.isEmpty{
     let currentPlace:[Int] = queue.removeFirst()
@@ -111,59 +119,59 @@ for _ in (입력된 문자열의 길이){
 //  지도 좌표는 인덱스 row+1, col+1부터 시작이다.
 //  */
 
-// //미로의 시작은 1,1 명시되어 있지만 2차원 배열의 처음 시작 인덱스는 0,0
-// let start = [0,0]
-// var queue: [[Int]] = []
-// var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: 6), count: 4)
-// var distances: [[Int]] = Array(repeating: Array(repeating: -1, count: 6), count: 4)
-
-// //목적지 좌표 입력 N(0번째 인덱스) M(1번째 인덱스)
-// var destination: [Int] = readLine()!.components(separatedBy: " ").map(){ Int($0) ?? 0}
-
-// //지도 입력
-// //difficult: 띄어쓰기 구분되어 있지 않은 입력을 어떻게 처리할 수 있을까?
-// var map = [[Int]]()
-// for _ in 0..<destination[0]{
-//        map.append(readLine()!.map({Int(String($0))!}))
-// }
-
-// queue.append(start)
-// //visited[start[0]][start[1]] = true
-
-// while !queue.isEmpty{
-//     print(queue)
-//     print(visited)
-//     let currentPlace:[Int] = queue.removeFirst()
-//     //방문하지 않았다면
-//     if (!visited[currentPlace[0]][currentPlace[1]]){
-//         //동서남북 다음 차례에 갈 수 있는 영역 찾기
-//         //difficult: out of range error 발생...
-//         //가독성 안좋음
-//         if(destination[1] > currentPlace[1]+1){
-//             if(map[currentPlace[0]][currentPlace[1]+1] == 1){
-//                 queue.append([currentPlace[0],currentPlace[1]+1])
-//                 distances[currentPlace[0]][currentPlace[1]+1] = distances[currentPlace[0]][currentPlace[1]] + 1
-//             }
-//         }
-//         if(destination[1] < currentPlace[1]-1){
-//             if(map[currentPlace[0]][currentPlace[1]-1] == 1){
-//                 queue.append([currentPlace[0],currentPlace[1]-1])
-//                 distances[currentPlace[0]][currentPlace[1]-1] = distances[currentPlace[0]][currentPlace[1]] + 1
-//             }
-//         }
-//         if(destination[0] < currentPlace[0]+1){
-//             if(map[currentPlace[0]+1][currentPlace[1]] == 1){
-//                 queue.append([currentPlace[0]+1,currentPlace[1]])
-//                 distances[currentPlace[0]+1][currentPlace[1]] = distances[currentPlace[0]][currentPlace[1]] + 1
-//             }
-//         }
-//         if(destination[0] < currentPlace[0]-1){
-//             if(map[currentPlace[0]-1][currentPlace[1]] == 1){
-//                 queue.append([currentPlace[0]-1,currentPlace[1]])
-//                 distances[currentPlace[0]-1][currentPlace[1]+1] = distances[currentPlace[0]][currentPlace[1]] + 1
-//             }
-//         }
-//     }
-// }
-
-// print(distances)
+//미로의 시작은 1,1 명시되어 있지만 2차원 배열의 처음 시작 인덱스는 0,0
+//let start = [0,0]
+//var queue: [[Int]] = []
+//var visited: [[Bool]] = Array(repeating: Array(repeating: false, count: 6), count: 4)
+//var distances: [[Int]] = Array(repeating: Array(repeating: -1, count: 6), count: 4)
+//
+////목적지 좌표 입력 N(0번째 인덱스) M(1번째 인덱스)
+//var destination: [Int] = readLine()!.components(separatedBy: " ").map(){ Int($0) ?? 0}
+//
+////지도 입력
+////difficult: 띄어쓰기 구분되어 있지 않은 입력을 어떻게 처리할 수 있을까?
+//var map = [[Int]]()
+//for _ in 0..<destination[0]{
+//       map.append(readLine()!.map({Int(String($0))!}))
+//}
+//
+//queue.append(start)
+////visited[start[0]][start[1]] = true
+//
+//while !queue.isEmpty{
+//    // print(queue)
+//    // print(visited)
+//    let currentPlace:[Int] = queue.removeFirst()
+//    //방문하지 않았다면
+//    if (!visited[currentPlace[0]][currentPlace[1]]){
+//        //동서남북 다음 차례에 갈 수 있는 영역 찾기
+//        //difficult: out of range error 발생...
+//        //가독성 안좋음
+//        if(destination[1] > currentPlace[1]+1){
+//            if(map[currentPlace[0]][currentPlace[1]+1] == 1){
+//                queue.append([currentPlace[0],currentPlace[1]+1])
+//                distances[currentPlace[0]][currentPlace[1]+1] = distances[currentPlace[0]][currentPlace[1]] + 1
+//            }
+//        }
+//        if(destination[1] < currentPlace[1]-1){
+//            if(map[currentPlace[0]][currentPlace[1]-1] == 1){
+//                queue.append([currentPlace[0],currentPlace[1]-1])
+//                distances[currentPlace[0]][currentPlace[1]-1] = distances[currentPlace[0]][currentPlace[1]] + 1
+//            }
+//        }
+//        if(destination[0] < currentPlace[0]+1){
+//            if(map[currentPlace[0]+1][currentPlace[1]] == 1){
+//                queue.append([currentPlace[0]+1,currentPlace[1]])
+//                distances[currentPlace[0]+1][currentPlace[1]] = distances[currentPlace[0]][currentPlace[1]] + 1
+//            }
+//        }
+//        if(destination[0] < currentPlace[0]-1){
+//            if(map[currentPlace[0]-1][currentPlace[1]] == 1){
+//                queue.append([currentPlace[0]-1,currentPlace[1]])
+//                distances[currentPlace[0]-1][currentPlace[1]+1] = distances[currentPlace[0]][currentPlace[1]] + 1
+//            }
+//        }
+//    }
+//}
+//
+//print(distances)
