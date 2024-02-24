75c98eba52df0d3502df06a0b624de9ad10c70fc
DongjuPark <pdj95044@gmail.com>
2024-02-24 13:58:16 +0900

    [백준] 불 문제풀이

--- BFS/fire.swift
+++ BFS/fire.swift
@@ -1,9 +1,36 @@
-let impossible: String = "IMPOSSIBLE"
-let WALL = -2
-let SPACE = 0
-let ME = -1
-let FIRE = 1
+/*
+ [백준] 불
+ 
+ 놓친 부분
+ 탈출자의 현재 시간을 비교할 것이 아니라 이동한 후 즉 이동하려는 위치까지 이동을 하면서 발생한 시간을 반영하고
+ 불이 해당 지역까지 도달하는데 걸린 시간과 탈출자가 동일한 위치까지 도달하는데 걸린 시간을 비교했어야했다.
+ if(두 시간이 같은 경우) -> 해당 지역에 불과 탈출자가 동시에 도착
+ if(탈출자의 시간값이 더 큰 경우) -> 탈출자가 해당 지역에 도착하기 전에 불이 이미 났다는 것을 의미
+ 
+  ....F
+  ...J#
+  ....#
+  ....#
+  ...#.
 
+  (spreadFire)
+  5 4 3 2 1
+  4 3 2 @ #
+  7 6 5 4 #
+  8 7 4 3 #
+  9 8 7 # 100
+
+  F.F
+  .J.
+  F.F
+
+  (spreadFire)
+  1 2 1
+  2 @ 2
+  1 2 1
+ */
+
+let impossible: String = "IMPOSSIBLE"
 let SC: Int = Int(readLine()!)!
 
 func spreadFire(fireMap: inout [[Int]], height: Int, width: Int){
@@ -15,12 +42,12 @@ func spreadFire(fireMap: inout [[Int]], height: Int, width: Int){
     //불 위치 enqueue
     for y in 0..<height{
         for x in 0..<width{
-            if fireMap[y][x] == FIRE{
+            if fireMap[y][x] == 1{
                 queue.append((y,x))
             }
-            if fireMap[y][x] == ME{
+            if fireMap[y][x] == 2{
                 currentPos.append(contentsOf: [y,x])
-                fireMap[y][x] = SPACE
+                fireMap[y][x] = 0
             }
         }
     }
@@ -53,8 +80,7 @@ func spreadFire(fireMap: inout [[Int]], height: Int, width: Int){
             }
         }
     }
-            
-    fireMap[currentPos[0]][currentPos[1]] = ME
+
 }
 func isSpaceForEscape(fireMap:[[Int]],height: Int, width: Int) -> Bool{
     //지도 외곽에 탈출할 수 있는 공간이 없는 경우
@@ -114,7 +140,10 @@ func findRoute(fireMap: inout [[Int]], height: Int, width: Int) -> Int?{
             let ny = y + dy[i]
             
             if nx >= 0 && nx < width && ny >= 0 && ny < height{
-                if fireMap[ny][nx] > fireMap[y][x]{
+                
+//              틀렸던 부분
+//              if fireMap[ny][nx] > fireMap[y][x]{
+                if fireMap[ny][nx] > fireMap[y][x] + 1{
                     fireMap[ny][nx] = fireMap[y][x] + 1
                     queue.append((ny,nx))
                 }
@@ -133,34 +162,26 @@ for _ in 0..<SC{
     let h = hw[1]
     let w = hw[0]
 
-
     var fireMap: [[Int]] = Array(repeating: [], count: h)
 
-
     for height in 0..<h{
-        let temp = readLine()!.map(){
+        fireMap[height].append(contentsOf: readLine()!.map(){
             if $0 == "#"{
-                return WALL
+                return -2
             }
             else if $0 == "."{
-                return SPACE
+                return 0
             }
             else if $0 == "@"{
-                return ME
+                return -1
             }
             else {
-                return FIRE
+                return 1
             }
-        }
-        fireMap[height].append(contentsOf: temp)
+        })
     }
-   
     
-    if !isSpaceForEscape(fireMap: fireMap, height: h, width: w){ continue }
-
     spreadFire(fireMap: &fireMap, height: h, width: w)
-    
     print(findRoute(fireMap: &fireMap, height: h, width: w) ?? impossible)
     
 }
-
