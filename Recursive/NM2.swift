f5ee20ebc3a79d40fed8eb97bca8ca7bdfbe9a62
DongjuPark <pdj95044@gmail.com>
2024-03-06 12:25:49 +0900

    [백준] N과 M(5) 문제풀이

---
+++ Recursive/NM2.swift
@@ -0,0 +1,30 @@
+
+/*
+ [백준] N과 M (5)
+ */
+
+let NM = readLine()!.split(separator: " ").map{Int(String($0))!}
+let N = NM[0]
+let M = NM[1]
+
+var nums = readLine()!.split(separator: " ").map{Int(String($0))!}
+nums = nums.sorted{$0 < $1}
+
+var visited: [Bool] = Array(repeating: false, count: nums.count)
+var stack = [Int]()
+
+func dfs(){
+    if stack.count == M{
+        print(stack.map{String($0)}.joined(separator: " "))
+        return
+    }
+    for idx in 0..<nums.count{
+        if !stack.contains(nums[idx]){
+            stack.append(nums[idx])
+            dfs()
+            stack.removeLast()
+        }
+    }
+}
+
+dfs()
