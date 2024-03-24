139b349a1879dfc0e3c855ab1d2cb0493fcd632c
DongjuPark <pdj95044@gmail.com>
2024-03-24 11:19:57 +0900

    [백준] 캠핑 문제풀이(틀림)

---
+++ Greedy/camping.swift
@@ -0,0 +1,43 @@
+/*
+ [백준] 캠핑
+ 
+ 20일 중 10일 사용 가능
+ 
+ 28일 휴가
+ 
+ P = 전체 일 수
+ L = 연속해서 사용 가능한 일 수
+ V = 일 일 수
+ */
+var cnt = 0
+
+var L = 0
+var P = 0
+var V = 0
+
+func recursive(dayOff: Int){
+    if dayOff - P < 0{
+        numOfCamping += dayOff
+        return
+    }
+    numOfCamping += L
+    recursive(dayOff: dayOff - P)
+}
+
+var numOfCamping = 0
+while true{
+    let LPV = readLine()!.split(separator: " ").map{Int(String($0))!}
+    L = LPV[0]
+    P = LPV[1]
+    V = LPV[2]
+    cnt += 1
+    if L == 0 && P == 0 && V == 0 { break }
+    var result = 0
+    
+    recursive(dayOff: V)
+
+    print("Case \(cnt): \(numOfCamping)")
+    numOfCamping = 0
+}
+
+
